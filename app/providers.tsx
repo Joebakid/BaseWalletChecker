0061b9feafc69f16bfcc654321101d10aa7697ad
joebakid <josephbawo@gmail.com>
2025-10-21 22:59:34 +0300

    provider deleted

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 // import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { wagmiConfig } from "@/lib/wagmi";
+// import { wagmiConfig } from "@/lib/wagmi";
 
 const queryClient = new QueryClient();
 
