eb89365b89e5ace5f40bde3f07a7c13d2faa46b0
joebakid <josephbawo@gmail.com>
2025-10-29 00:13:04 +0200

    task

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,41 +1,26 @@
-// minikit.config.ts
-
 export const minikitConfig = {
- 
   "accountAssociation": {
     "header": "eyJmaWQiOjExMzc4NTAsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg3REU4YjljYTNhNDFjMDcyMWQzOUZBRmVhN2Q0ZjY0MDUzZDREZjUxIn0",
     "payload": "eyJkb21haW4iOiJiYXNlLXdhbGxldGNoZWNrZXIudmVyY2VsLmFwcCJ9",
     "signature": "8kKlE+aniNmV0AuZchf+z3QdzbgI9M2G/tiTRlWKAjllDZb+emJKXbE5ovYE6MT6kf+NsDM3HoG4YZcwILNToxw="
-  }
-
-,
-
-  baseBuilder: {
-    ownerAddress: "0x1c08A8A497ea9E2aAE3EE9d9b8e8b20D426DA085", // <-- replace with your Base account address
   },
-
   miniapp: {
     version: "1",
-    name: "Base Wallet Checker",
-    subtitle: "Quick wallet insights",
-    description:
-      "Analyze Base wallet activity quickly — dApps, tokens, fees & volume.",
-    screenshotUrls: [
-      `${process.env.NEXT_PUBLIC_ROOT_URL}/og.png`,
-    ],
-    iconUrl: `${process.env.NEXT_PUBLIC_ROOT_URL}/icon.png`,
-    splashImageUrl: `${process.env.NEXT_PUBLIC_ROOT_URL}/og.png`,
+    name: "Cubey", 
+    subtitle: "Your AI Ad Companion", 
+    description: "Ads",
+    screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
+    iconUrl: `${ROOT_URL}/blue-icon.png`,
+    splashImageUrl: `${ROOT_URL}/blue-hero.png`,
     splashBackgroundColor: "#000000",
-    homeUrl: process.env.NEXT_PUBLIC_ROOT_URL,
-    webhookUrl: `${process.env.NEXT_PUBLIC_ROOT_URL}/api/webhook`,
-    primaryCategory: "utilities",
-    tags: ["base", "wallet", "analytics"],
-    heroImageUrl: `${process.env.NEXT_PUBLIC_ROOT_URL}/og.png`,
-    tagline: "Check Base activity at a glance",
-    ogTitle: "Base Wallet Checker",
-    ogDescription:
-      "Analyze Base wallet activity quickly — track dApps, tokens, fees & more.",
-    ogImageUrl: `${process.env.NEXT_PUBLIC_ROOT_URL}/og.png`,
-    noindex: true, // optional, hides from search if you want
+    homeUrl: ROOT_URL,
+    webhookUrl: `${ROOT_URL}/api/webhook`,
+    primaryCategory: "social",
+    tags: ["marketing", "ads", "quickstart", "waitlist"],
+    heroImageUrl: `${ROOT_URL}/blue-hero.png`, 
+    tagline: "",
+    ogTitle: "",
+    ogDescription: "",
+    ogImageUrl: `${ROOT_URL}/blue-hero.png`,
   },
 } as const;
